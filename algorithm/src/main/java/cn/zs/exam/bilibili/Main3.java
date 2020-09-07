a0609a35aa23179baa1e8c6d2ae66419d587bffc
notebook <1150087076@qq.com>
2020-09-07 10:35:23 +0800

    各路笔试

--- algorithm/src/main/java/cn/zs/exam/bilibili/Main3.java
+++ algorithm/src/main/java/cn/zs/exam/bilibili/Main3.java
@@ -6,7 +6,7 @@ package cn.zs.exam.bilibili;
 
 import java.util.ArrayList;
 ////[[1, 2, 3, 4], [5, 6, 7, 8], [9,10,11,12] ]
-public class Main4 {
+public class Main3 {
     public static void main(String[] args) {
         int [][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         int[] ints = printMatrix(matrix);
