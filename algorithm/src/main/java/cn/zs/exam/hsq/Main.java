465f2e041a83707160e764e7b2a9983934b536e5
bbqqhsq <1074940398hsq@gmail.com>
2020-09-12 23:03:19 +0800

    hello hsq

---
+++ algorithm/src/main/java/cn/zs/exam/hsq/Main.java
@@ -0,0 +1,14 @@
+package cn.zs.exam.hsq;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author huanshunqi
+ * @Date 2020/9/12 23:02
+ * @Version 1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hsq");
+    }
+}
