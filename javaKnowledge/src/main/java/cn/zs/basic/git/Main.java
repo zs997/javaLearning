eab1ccdf0892d998678661c861be7675235db405
hust <zhushuai1997@qq.com>
2020-11-24 20:41:30 +0800

    test

---
+++ javaKnowledge/src/main/java/cn/zs/basic/git/Main.java
@@ -0,0 +1,15 @@
+package cn.zs.basic.git;
+
+public class Main {
+
+    /**
+     * 与GitHub远程仓库关联
+     * github远程项目 可以ssh下载 可以https下载
+     * https下载不需要本人登录github，下载后 每次push都需要 输入账户密码
+     * 这时候如果idea登录了GitHub账户 也可以直接push 也会跳出账户密码输入栏
+     *
+     * 如果git使用 ssh 关联了GitHub账户（一台计算机一个关联），从此计算机上，使用ssh下载的项目，每次都可以直接push
+     *
+     */
+
+}
